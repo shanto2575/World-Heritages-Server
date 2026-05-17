793ea45a24d0db7067f1111725e2b17cdf9146bd
shanto2575 <shanto2575@gmail.com>
2026-05-17 12:09:51 +0600

    jwt verify

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const port = process.env.PORT
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs')
 const uri = process.env.MONGODB_URI;
 
 app.use(cors())
@@ -18,48 +19,93 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+const JWKS = createRemoteJWKSet(
+    new URL('http://localhost:3000/api/auth/jwks')
+)
+const verifyToken = async (req, res, next) => {
+    const authHeader = req?.headers.authorization;
+    const token = authHeader?.split(' ')[1]
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+    try {
+        const { payload } = await jwtVerify(token, JWKS)
+        console.log(payload)
+        next()
+    } catch (error) {
+        return res.status(403).json({ message: 'Forbidden' })
+    }
 
+}
 async function run() {
     try {
         await client.connect();
-        const db=client.db('World-Heritage-Site')
-        const heritageCollection=db.collection('heritages')
-        
-        app.get('/heritages',async(req,res)=>{
-            const result=await heritageCollection.find().toArray()
+        const db = client.db('World-Heritage-Site')
+        const heritageCollection = db.collection('heritages')
+        const bookingCollection = db.collection('booking')
+
+        app.get('/feature',async(req,res)=>{
+            const result=await heritageCollection.find().limit(3).toArray()
+            res.json(result)
+        })
+        app.get('/heritages', async (req, res) => {
+            const result = await heritageCollection.find().toArray()
             res.json(result)
         })
 
-        app.get('/heritages/:id',async(req,res)=>{
-            const {id}=req.params;
-            const result=await heritageCollection.findOne({_id:new ObjectId(id)})
+        app.get('/heritages/:id', verifyToken, async (req, res) => {
+            const { id } = req.params;
+            const result = await heritageCollection.findOne({ _id: new ObjectId(id) })
             // console.log(result)
             res.json(result)
         })
-        
-        app.post('/heritages',async(req,res)=>{
-            const heritageData=req.body;
-            const result=await heritageCollection.insertOne(heritageData)
-            console.log(result,'result')
+
+        app.post('/heritages', verifyToken, async (req, res) => {
+            const heritageData = req.body;
+            const result = await heritageCollection.insertOne(heritageData)
+            console.log(result, 'result')
             res.json(result)
         })
 
-        app.patch('/heritages/:id',async(req,res)=>{
-            const {id}=req.params;
-            const update=req.body;
-            const result=await heritageCollection.updateOne(
-                {_id:new ObjectId(id)},
-                {$set:update}
+        app.patch('/heritages/:id', verifyToken, async (req, res) => {
+            const { id } = req.params;
+            const update = req.body;
+            const result = await heritageCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: update }
             )
             res.json(result)
             // console.log(result,'update result')
         })
 
-        app.delete('/heritages/:id',async(req,res)=>{
-            const {id}=req.params;
-            const result=await heritageCollection.deleteOne({_id:new ObjectId(id)})
+        app.delete('/heritages/:id', verifyToken, async (req, res) => {
+            const { id } = req.params;
+            const result = await heritageCollection.deleteOne({ _id: new ObjectId(id) })
             res.json(result)
             // console.log(result,'delete result')
+        })
+
+        app.get('/booking/:userId',verifyToken,async(req,res)=>{
+            const {userId}=req.params;
+            const result=await bookingCollection.find({userId:userId}).toArray()
+            res.json(result)
+            // console.log('booking result',result)
+        })
+
+        app.post('/booking',verifyToken,async(req,res)=>{
+            const bookingdata=req.body;
+            const result=await bookingCollection.insertOne(bookingdata)
+            res.json(result)
+            // console.log(result,'booking data')
+        })
+
+        app.delete('/booking/:id',verifyToken,async(req,res)=>{
+            const {id}=req.params;
+            const result=await bookingCollection.deleteOne({_id:new ObjectId(id)})
+            res.json(result)
         })
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
